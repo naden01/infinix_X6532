3e8f12d768c1f14bbbdd9e50bab37dfafb8c4b5a
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-10-15 09:31:50 -0700

    push tree for x6532
    Co-Authored-By: Smiley900 <24617731+smiley900@users.noreply.github.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_X6532.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_X6532-user \
+    twrp_X6532-userdebug \
+    twrp_X6532-eng
