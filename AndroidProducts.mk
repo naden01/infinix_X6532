cbb3b7d4bc2f8a3ecea34a06adb2de098d3a732f
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-10-16 12:06:52 +0700

    Update product makefiles and lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_X6532.mk
+    $(LOCAL_DIR)/pb_X6532.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_X6532-user \
-    twrp_X6532-userdebug \
-    twrp_X6532-eng
+    pb_X6532-user \
+    pb_X6532-userdebug \
+    pb_X6532-eng
